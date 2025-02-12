3c213a8184757a57a769ba5d6dce33905adb1cea
shunyonai1996 <y_hhns_1028@icloud.com>
2025-02-13 08:12:35 +0900

    ディレクトリ構成の修正等

--- February/2025-02-12_ABC392_A.py
+++ February/2025-02-12_ABC392_A.py
@@ -1,5 +1,5 @@
 import io,sys
-with open("input.txt") as TxtOpen:
+with open("../input.txt") as TxtOpen:
     INPUT=TxtOpen.read()
 sys.stdin=io.StringIO(INPUT)
 # --------------------------------------------------------
