cf03a3d8df0a50607d59d4af2d2a0ed883c4cdd5
shunyonai1996 <y_hhns_1028@icloud.com>
2025-02-28 08:32:37 +0900

    ABC385_B 不正解

--- February/2025-02-28_ABC385_B.py
+++ February/2025-02-28_ABC385_B.py
@@ -5,5 +5,39 @@ sys.stdin=io.StringIO(INPUT)
 # --------------------------------------------------------
 
 # issue
-# 
+# XYの初期値を変数で保持
+# 地図をマッピング
+# 最終行の入力によって、XYを移動
+H, W, X, Y = list(map(int, input().split()))
+santa = [X, Y]
+S = []
+present = 0
 
+for i in range(H):
+    S.append(list(input()))
+
+T = list(input())
+for value in T:
+    if S[santa[0]][santa[1]] == '@':
+        S[santa[0]][santa[1]] = 'v'
+        present += 1
+    if value == 'U':
+        print(S[santa[0]][santa[1]])
+        santa = [santa[0]-1,santa[1]]
+        if S[santa[0]-1][santa[1]] == '#':
+            continue
+    elif value == 'D':
+        if S[santa[0]+1][santa[1]] == '#':
+            continue
+        santa = [santa[0]+1,santa[1]]
+    elif value == 'L':
+        if S[santa[0]][santa[1]-1] == '#':
+            continue
+        santa = [santa[0],santa[1]-1]
+    elif value == 'R':
+        if S[santa[0]][santa[1]+1] == '#':
+            continue
+        santa = [santa[0],santa[1]+1]
+    print(present)
+
+print(f'{santa[0]+1} {santa[1]+1} {present}')
