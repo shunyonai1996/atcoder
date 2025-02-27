a4cd518ed89b04447b3ddc4c0c37f411f7d0001a
shunyonai1996 <y_hhns_1028@icloud.com>
2025-02-28 06:54:22 +0900

    ABC385_A 提出

---
+++ February/2025-02-28_ABC385_D.py
@@ -0,0 +1,9 @@
+import io,sys
+with open("../input.txt") as TxtOpen:
+    INPUT=TxtOpen.read()
+sys.stdin=io.StringIO(INPUT)
+# --------------------------------------------------------
+
+# issue
+# 
+
