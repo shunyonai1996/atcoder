dcf8c36b549a65efe9b5d8268f0569b744951f2f
shunyonai1996 <y_hhns_1028@icloud.com>
2025-03-04 08:19:57 +0900

    ABC393_A 解答

---
+++ March/2025-03-04_ABC393_A.py
@@ -0,0 +1,21 @@
+import io,sys
+with open("../input.txt") as TxtOpen:
+    INPUT=TxtOpen.read()
+sys.stdin=io.StringIO(INPUT)
+# --------------------------------------------------------
+
+# sick sick -> 1が毒
+# sick fine -> 2が毒
+# fine sick -> 3が毒
+# fine fine -> 4が毒
+
+S = input()
+
+if S == 'sick sick':
+    print(1)
+elif S == 'sick fine':
+    print(2)
+elif S == 'fine sick':
+    print(3)
+elif S == 'fine fine':
+    print(4)
