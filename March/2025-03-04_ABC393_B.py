97b62550c3202b38ca6363d3fc4f2a9f3a1af1a2
shunyonai1996 <y_hhns_1028@icloud.com>
2025-03-04 21:47:46 +0900

    ABC393_B 解説参照

--- March/2025-03-04_ABC393_B.py
+++ March/2025-03-04_ABC393_B.py
@@ -21,3 +21,17 @@ for i, v in enumerate(S):
                 if S[index] == 'B' and S[2*index] == 'C':
                     ans += 1
 print(ans)
+
+
+# 解説による解答
+s = input()
+n = len(s)
+ans = 0
+
+for i in range(n):
+  for j in range(i + 1, n):
+    for k in range(j + 1, n):
+      if j - i == k - j and s[i] == 'A' and s[j] == 'B' and s[k] == 'C':
+        ans += 1
+        
+print(ans)
