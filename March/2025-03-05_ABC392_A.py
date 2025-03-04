f805fb99fdac39868f8464940e6f18fafaf14909
shunyonai1996 <y_hhns_1028@icloud.com>
2025-03-05 08:26:36 +0900

    ABC392_A 解答

---
+++ March/2025-03-05_ABC392_A.py
@@ -0,0 +1,8 @@
+import io,sys
+with open("../input.txt") as TxtOpen:
+    INPUT=TxtOpen.read()
+sys.stdin=io.StringIO(INPUT)
+# --------------------------------------------------------
+
+S = sorted(list(map(int, input().split())))
+print('Yes') if S[0] * S[1] == S[2] else print('No')
