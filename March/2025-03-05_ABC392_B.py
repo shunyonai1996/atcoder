e1a56b2058cd3ccaab3a0fdc88a259ba57e21eb7
Shun Yonai <78899768+shunyonai1996@users.noreply.github.com>
2025-03-05 17:25:58 +0900

    ABC392_B/AIのコードレビュー

--- March/2025-03-05_ABC392_B.py
+++ March/2025-03-05_ABC392_B.py
@@ -4,16 +4,16 @@ with open("../input.txt") as TxtOpen:
 sys.stdin=io.StringIO(INPUT)
 # --------------------------------------------------------
 
+# AIによるソースレビュー
+# listよりもsetのほうが高速（検索にハッシュテーブル使用する）
+# -> `not in S`でO(1)で検索できる※リストはO(N)
+# ループを内包表記で記述
+# 値が1つ以上あるかのチェックは比較演算子が不要
+
 N, M = map(int, input().split())
-S = list(map(int, input().split()))
-ans = []
+S = set(map(int, input().split()))
+ans = [i for i in range(1,N+1) if i not in S]
 
-for i in range(1, N+1):
-    if i not in S:
-        ans.append(i)
-
-if len(ans) > 0:
-    print(len(ans))
+print(len(ans))
+if ans:
     print(*ans)
-else:
-    print(0)
