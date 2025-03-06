f3728d86d67b4384c884f32afbcf1515cd24c599
shunyonai1996 <y_hhns_1028@icloud.com>
2025-03-07 08:46:41 +0900

    ABC_126/AIのコードレビュー

--- March/2025-03-07_ABC126_B.py
+++ March/2025-03-07_ABC126_B.py
@@ -13,6 +13,10 @@ sys.stdin=io.StringIO(INPUT)
 # S[0]、S[1]がともに00 or 13以上→NA
 # 出力パターン YYMM, MMYY, AMBIGUOUS, NA
 
+# AIによるソースレビュー
+# リスト表記はメモリ効率が悪いため、単一の整数を二つ定義すべき
+# 比較演算子の記法を統一する
+
 S = input()
 list_ = [int(S[i:i+2]) for i in range(0,4,2)]
 
@@ -24,3 +28,28 @@ elif 1 <= list_[0] <= 12 and 1 <= list_[1] <= 12:
     print('AMBIGUOUS')
 else:
     print('NA')
+
+
+# AI提供のソースコード
+S = input()
+
+# スライスの省略記法S[:2],S[2:]
+first = int(S[:2])
+second = int(S[2:])
+
+if (first == 0 or first >= 13) and 1 <= second <= 12:
+    print('YYMM')
+elif 1 <= first <= 12 and (second == 0 or second >= 13):
+    print('MMYY')
+elif 1 <= first <= 12 and 1 <= second <= 12:
+    print('AMBIGUOUS')
+else:
+    print('NA')
+
+# AI提供の最大限記述を簡略化
+S=input()
+a,b=int(S[:2]),int(S[2:])
+if(a==0 or a>12)and 0<b<13:print('YYMM')
+elif 0<a<13 and(b==0 or b>12):print('MMYY')
+elif 0<a<13 and 0<b<13:print('AMBIGUOUS')
+else:print('NA')
