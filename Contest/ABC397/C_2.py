ef54570ec2669c367024cc3d6a38bd5cfab0192f
shunyonai1996 <y_hhns_1028@icloud.com>
2025-03-16 13:27:31 +0900

    ABC397_C_2

---
+++ Contest/ABC397/C_2.py
@@ -0,0 +1,32 @@
+import io,sys
+with open("./input.txt") as TxtOpen:
+    INPUT=TxtOpen.read()
+sys.stdin=io.StringIO(INPUT)
+# --------------------------------------------------------
+
+N = int(input())
+A = list (map(int, input().split()))
+prefix = [0] * N
+suffix = [0] * N
+ans = 0
+count = 0
+seen = set()
+
+for i in range(N):
+    if A[i] not in seen:
+        seen.add(A[i])
+        count += 1
+    prefix[i] = count
+
+# suffix = list(reversed(prefix))でも良いが、累積和の逆転の考え方を使用するため、以下の記載をしている
+seen = set()
+count = 0
+for i in range(N-1, -1, -1):
+    if A[i] not in seen:
+        seen.add(A[i])
+        count += 1
+    suffix[i] = count
+
+for i in range(N-1):
+    ans = max(ans, prefix[i] + suffix[i+1])
+print(ans)
