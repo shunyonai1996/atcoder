4c6a638d19421925f322cbaedbbf447a0adc7d6e
shunyonai1996 <y_hhns_1028@icloud.com>
2025-03-16 11:42:02 +0900

    ABC397_C

--- Contest/ABC397/C.py
+++ Contest/ABC397/C.py
@@ -15,19 +15,40 @@ sys.stdin=io.StringIO(INPUT)
 
 
 # 解説
+# 事前にprefixとsuffixのリストを作成し、i回目とi+1回目の種類数を作成しておく
+# のぞいたそれぞれのリストを使ってN回ループを回す。
+# 区切った位置の中で種類が最大になるパターンを抽出
+
 N = int(input())
 A = list(map(int, input().split()))
 
-S = 0
-V = [0]*N
-print(S, V)
+seen = set()
+prefix = [0] * (N)
+count = 0
+for i in range(N):
+    if A[i] not in seen:
+        seen.add(A[i])
+        count += 1
+    prefix[i] = count
 
-for i in range(N):
-    if V[A[i]] == 0:
-        V[A[i]] = 1
-        S += 1
-for i in range(N):
-    if V[A[i]] == 0:
-        V[A[i]] = 1
-        S += 1
-print(S, V)
+# 今回は以下のコメントアウトの記載でも同様の結果が得られる
+# ※ただし、累積和の計算によっては結果が異なるケースもあるため非推奨
+# suffix = list(reversed(prefix))
+seen = set()
+suffix = [0] * (N)
+count = 0
+
+for i in range(N-1, -1, -1):
+    if A[i] not in seen:
+        seen.add(A[i])
+        count += 1
+    suffix[i] = count
+    print(i)
+    print(seen)
+    print(suffix)
+
+print(prefix,suffix)
+ans = 0
+for i in range(N-1):
+    ans = max(ans, prefix[i] + suffix[i+1])
+print(ans)
