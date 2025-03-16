8a9ec839ecef9e05a70b7aaa2f55c92cf9e3c904
shunyonai1996 <y_hhns_1028@icloud.com>
2025-03-16 09:06:39 +0900

    ABC_397_B

--- Contest/ABC397/B.py
+++ Contest/ABC397/B.py
@@ -4,20 +4,36 @@ with open("./input.txt") as TxtOpen:
 sys.stdin=io.StringIO(INPUT)
 # --------------------------------------------------------
 
-S = list(map(str, input()))
+# S = list(map(str, input()))
+# ans = 0
+
+# for index in range(1, 200):
+#     if index % 2 == 0:
+#         if S[index - 1] != 'o':
+#             S.insert(index - 1, 'o')
+#             ans += 1
+#     else:
+#         if S[index - 1] != 'i':
+#             S.insert(index - 1, 'i')
+#             ans += 1
+#     if index == len(S):
+#         if len(S) % 2 != 0: ans += 1
+#         break
+
+# print(ans)
+
+# 解説
+# 初期値をiとし、iとoを反転させ、Sと一致するかどうか判定
+
+S = list(input())
+value = 'i'
 ans = 0
 
-for index in range(1, 200):
-    if index % 2 == 0:
-        if S[index - 1] != 'o':
-            S.insert(index - 1, 'o')
-            ans += 1
+for i in S:
+    if i == value:
+        value = 'o' if value == 'i' else 'i'
     else:
-        if S[index - 1] != 'i':
-            S.insert(index - 1, 'i')
-            ans += 1
-    if index == len(S):
-        if len(S) % 2 != 0: ans += 1
-        break
+        ans += 1
 
+if value == 'o': ans += 1
 print(ans)
--- Contest/ABC397/C.py
+++ Contest/ABC397/C.py
@@ -4,11 +4,30 @@ with open("./input.txt") as TxtOpen:
 sys.stdin=io.StringIO(INPUT)
 # --------------------------------------------------------
 
+# N = int(input())
+# A = list(map(int, input().split()))
+
+# ans = 0
+# for i in range(N):
+#     if ans < len(set(A[:i])) + len(set(A[i:])):
+#         ans = len(set(A[:i])) + len(set(A[i:]))
+# print(ans)
+
+
+# 解説
 N = int(input())
 A = list(map(int, input().split()))
 
-ans = 0
+S = 0
+V = [0]*N
+print(S, V)
+
 for i in range(N):
-    if ans < len(set(A[:i])) + len(set(A[i:])):
-        ans = len(set(A[:i])) + len(set(A[i:]))
-print(ans)
+    if V[A[i]] == 0:
+        V[A[i]] = 1
+        S += 1
+for i in range(N):
+    if V[A[i]] == 0:
+        V[A[i]] = 1
+        S += 1
+print(S, V)
