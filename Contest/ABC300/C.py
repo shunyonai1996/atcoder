b0961115b722fc5717718b6b4743993a9d27c774
shunyonai1996 <y_hhns_1028@icloud.com>
2025-03-17 07:55:41 +0900

    ABC300_C

---
+++ Contest/ABC300/C.py
@@ -0,0 +1,12 @@
+import io,sys
+with open("./input.txt") as TxtOpen:
+    INPUT=TxtOpen.read()
+sys.stdin=io.StringIO(INPUT)
+# --------------------------------------------------------
+
+# issue
+# 問題の条件を満たしていること
+
+H, W = list(map(int, input().split()))
+N = min(H, W)
+print(H, W, N)
