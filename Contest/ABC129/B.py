4682f30495b85726350bb2a375a01897cd78db58
Shun Yonai <78899768+shunyonai1996@users.noreply.github.com>
2025-03-21 16:31:21 +0900

    ABC129_B

---
+++ Contest/ABC129/B.py
@@ -0,0 +1,17 @@
+import io,sys
+with open("./input.txt") as TxtOpen:
+    INPUT=TxtOpen.read()
+sys.stdin=io.StringIO(INPUT)
+# --------------------------------------------------------
+
+N = int(input())
+W = list(map(int, input().split()))
+
+ans_list = []
+for i in range(1, N):
+    first = W[:i]
+    second = W[i:]
+    total = sum(first) + sum(second)
+    ans_list.append(total)
+
+print(max(ans_list))
