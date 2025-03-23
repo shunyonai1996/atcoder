e7aec75117dc2fc3d061a7587179d77927a5b300
shunyonai1996 <y_hhns_1028@icloud.com>
2025-03-23 09:24:05 +0900

    ABC398_B

--- Contest/ABC398/A.py
+++ Contest/ABC398/A.py
@@ -14,3 +14,14 @@ else:
     ans[int((N-1) / 2)] = '='
 
 print(''.join(ans))
+
+# 解説記事
+# アプローチほぼ同じ
+# N=int(input())
+
+# if N%2==0:
+#   c = (N-2)//2
+#   print('-'*c + '='*2 + '-'*c)
+# else:
+#   c = (N-1)//2
+#   print('-'*c + '=' + '-'*c)
--- Contest/ABC398/B.py
+++ Contest/ABC398/B.py
@@ -4,7 +4,10 @@ with open("./input.txt") as TxtOpen:
 sys.stdin=io.StringIO(INPUT)
 # --------------------------------------------------------
 
-N = sorted(list(map(int, input().split())))
+# AIによるソースレビュー
+
+# N = sorted(list(map(int, input().split())))
+N = list(map(int, input().split()))
 T = [0]+[0] * 13
 
 for i in N:
@@ -23,3 +26,25 @@ if x and y:
     print('Yes')
 else:
     print('No')
+
+
+# AIによる別解
+N = list(map(int, input().split()))
+T = [0]*13
+
+for i in N:
+    T[i-1] += 1
+
+found = False
+
+# 全ての組み合わせを明示的に確認
+for i in range(13):
+    if T[i] >= 3:
+        for j in range(13):
+            if i != j and T[j] >= 2:
+                found = True
+                break
+        if found:
+            break
+
+print('Yes' if found else 'No')
