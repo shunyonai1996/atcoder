bd2af173d40a85f09e8c6b05d57ded5ecacae342
shunyonai1996 <y_hhns_1028@icloud.com>
2025-03-24 21:55:10 +0900

    ABC130_B

--- Contest/ABC130/B.py
+++ Contest/ABC130/B.py
@@ -5,7 +5,16 @@ sys.stdin=io.StringIO(INPUT)
 # --------------------------------------------------------
 
 N, X = list(map(int, input().split()))
-
 L = [int(l) for l in list(input().split())]
 
-print(N, X, L)
+pointer = 0
+answer = 1
+
+for i in range(N):
+    pointer += L[i]
+    if pointer <= X:
+        answer += 1
+    else:
+        break
+
+print(answer)
