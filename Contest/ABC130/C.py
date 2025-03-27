04f5f3c2cecba2877b026b328e7f18b236d7e8c9
shunyonai1996 <y_hhns_1028@icloud.com>
2025-03-27 21:55:19 +0900

    ABC130_C

--- Contest/ABC130/C.py
+++ Contest/ABC130/C.py
@@ -10,7 +10,17 @@ sep_min_x = min([x * H] + [(W - x) * H])
 sep_min_y = min([W * y] + [W * (H - y)])
 max_num = max(sep_min_x, sep_min_y)
 
-if sep_min_x == sep_min_y and (x != 0 and y != 0):
+if sep_min_x == sep_min_y and (x == 0 and y == 0):
     print(str('{:.07f}'.format(max_num)) + ' 1')
 else:
     print(str('{:.07f}'.format(max_num)) + ' 0')
+
+# AIの解説後再解答
+W, H, x, y = list(map(int, input().split()))
+
+min_size = W * H / 2
+
+if (W / 2, H / 2) == (x, y):
+    print(str('{:.7f}'.format(min_size)) + ' 1')
+else:
+    print(str('{:.7f}'.format(min_size)) + ' 0')
