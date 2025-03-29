f008746b2a629c4c2b6137d9a17ff41f76b2701d
shunyonai1996 <y_hhns_1028@icloud.com>
2025-03-29 19:44:13 +0900

    ABC399 Contest

---
+++ Contest/ABC399/B.py
@@ -0,0 +1,8 @@
+import io,sys
+with open("./input.txt") as TxtOpen:
+    INPUT=TxtOpen.read()
+sys.stdin=io.StringIO(INPUT)
+# --------------------------------------------------------
+
+N = list(map(int, input().split()))
+
