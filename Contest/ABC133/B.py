fc48c2a7b3539a0b9dbc7c6b3ecc82fdf9a22c5e
shunyonai1996 <y_hhns_1028@icloud.com>
2025-04-02 08:36:37 +0900

    ABC133_B

--- Contest/ABC133/B.py
+++ Contest/ABC133/B.py
@@ -4,3 +4,9 @@ with open("./input.txt") as TxtOpen:
 sys.stdin=io.StringIO(INPUT)
 # --------------------------------------------------------
 
+N, D = list(map(int, input().split()))
+X = [list(map(int, input().split())) for _ in range(N)]
+
+for i in range(N):
+    for j in range(D):
+        print(i, j)
