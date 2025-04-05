d87f5c33ffec9b32c95dfff5425b48534d0a5590
shunyonai1996 <y_hhns_1028@icloud.com>
2025-04-05 12:10:19 +0900

    ABC134_B

--- Contest/ABC134/B.py
+++ Contest/ABC134/B.py
@@ -4,3 +4,14 @@ with open("./input.txt") as TxtOpen:
 sys.stdin=io.StringIO(INPUT)
 # --------------------------------------------------------
 
+N, D = list(map(int, input().split()))
+if N <= D:
+    print('1')
+else:
+    ans = 0
+    for i in range(0, N, D*2+1):
+        if i + (D * 2) < N:
+            ans += 1
+        else:
+            ans += 1
+    print(ans)
