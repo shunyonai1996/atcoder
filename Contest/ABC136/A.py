b2e792e600d59d44e97b5683f4f6918009ea31c5
shunyonai1996 <y_hhns_1028@icloud.com>
2025-04-08 22:16:04 +0900

    ABC136_A

---
+++ Contest/ABC136/A.py
@@ -0,0 +1,12 @@
+import io,sys
+with open("./input.txt") as TxtOpen:
+    INPUT=TxtOpen.read()
+sys.stdin=io.StringIO(INPUT)
+# --------------------------------------------------------
+
+A, B, C = list(map(int, input().split()))
+
+if (C - (A - B)) > 0:
+    print(C - (A - B))
+else:
+    print(0)
