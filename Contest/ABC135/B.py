d5564e7ec4111ea032d14c09dbd44d9a5596ca93
shunyonai1996 <y_hhns_1028@icloud.com>
2025-04-21 08:30:43 +0900

    ABC_135_B

---
+++ Contest/ABC135/B.py
@@ -0,0 +1,19 @@
+import io,sys
+with open("./input.txt") as TxtOpen:
+    INPUT=TxtOpen.read()
+sys.stdin=io.StringIO(INPUT)
+# --------------------------------------------------------
+
+# ループをカウントアップしていく
+# それを入力と比較して、一致しない数を数える
+# 3以上なら'NO'2以下なら'YES'
+
+N = int(input())
+P = list(map(int, input().split()))
+diff_cnt = 0
+
+for i in range(1, N+1):
+    if i == P[i-1]:
+        diff_cnt += 1
+
+print('YES') if 3 <= diff_cnt else print('NO')
--- Contest/ABC148/A.py
+++ Contest/ABC148/A.py
@@ -4,7 +4,11 @@ with open("./input.txt") as TxtOpen:
 sys.stdin=io.StringIO(INPUT)
 # --------------------------------------------------------
 
-num_list = [1,2,3]
-input_list = {int(input()) for _ in range(2)}
-ans = [v for v in num_list if v not in input_list]
-print(ans[0])
+def main():
+    num_list = [1,2,3]
+    input_list = {int(input()) for _ in range(2)}
+    ans = [v for v in num_list if v not in input_list]
+    print(ans[0])
+
+if __name__ == "__main__":
+    main()
