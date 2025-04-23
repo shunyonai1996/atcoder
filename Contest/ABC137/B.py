e05736b23c6f9217d32d8cb61f9a355b7f8d09ae
shunyonai1996 <y_hhns_1028@icloud.com>
2025-04-24 08:24:55 +0900

    ABC138_B

--- Contest/ABC137/B.py
+++ Contest/ABC137/B.py
@@ -5,7 +5,12 @@ sys.stdin=io.StringIO(INPUT)
 # --------------------------------------------------------
 
 # Xの座標を起点に前後K個の座標を順番に出力する
-K, X = list(map(int, input().split()))
-min_val, max_val = X-K+1,X+K-1
+# K, X = list(map(int, input().split()))
+# min_val, max_val = X-K+1,X+K-1
 
-[print(i) for i in range(min_val, max_val+1)]
+# [print(i) for i in range(min_val, max_val+1)]
+
+# 再検証
+K, X = map(int, input().split())
+ans = ' '.join(map(str, range(X-K+1, X+K)))
+print(ans)
