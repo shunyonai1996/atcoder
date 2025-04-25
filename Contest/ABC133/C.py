f16e111be832c11c5067c6dde6c3a533b9568d90
shunyonai1996 <y_hhns_1028@icloud.com>
2025-04-25 13:06:09 +0900

    ABC133_C

--- Contest/ABC133/C.py
+++ Contest/ABC133/C.py
@@ -4,20 +4,40 @@ with open("./input.txt") as TxtOpen:
 sys.stdin=io.StringIO(INPUT)
 # --------------------------------------------------------
 
-# Lが2019未満 Rが2019未満
-# Lが2019未満 Rが2019以上
-# Lが2019以上 Rが2019未満
-# Lが2019以上 Rが2019以上
-
 L, R = map(int, input().split())
-min_val = 2019
-print(47*48%2019)
-# i, i+1はWA
-# i, i+nにすること
+min_val = float('inf')
 
-for i in range(L, R):
-    min_val = min(min_val, (i * (i + 1)) % 2019)
-    if min_val == 0:
-        break
+if 2019 < R - L:
+    loop_cnt = L + 2019
+else:
+    loop_cnt = R
 
+for i in range(L, loop_cnt):
+    for j in range(L, loop_cnt):
+        min_val = min(min_val, (i * (j + 1)) % 2019)
+        if min_val == 0:
+            break
 print(min_val)
+
+
+# 解説記事をpython変換
+L = 0
+R = 0
+inf = 10**9
+
+def chmin(a, b):
+    return b if b < a else a
+
+def solve():
+    ans = inf
+    for i in range(L, R):
+        for j in range(i + 1, R + 1):
+            ans = chmin(ans, (i * j) % 2019)
+            if ans == 0:
+                return 0
+    return ans
+
+def main():
+    global L, R
+    L, R = map(int, input().split())
+    print(solve())
