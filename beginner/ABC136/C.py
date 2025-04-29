5041d0efd3962ce9fb358a4e8263d73f7d787af7
shunyonai1996 <y_hhns_1028@icloud.com>
2025-04-30 07:18:55 +0900

    ABC137_C

--- beginner/ABC136/C.py
+++ beginner/ABC136/C.py
@@ -4,15 +4,3 @@ with open("/Users/shunyonai/Documents/GitHub/competitive-programming/input.txt")
 sys.stdin=io.StringIO(INPUT)
 # --------------------------------------------------------
 
-import sys
-input = sys.stdin.readline
-
-N = int(input())
-ans = 0
-s = [sorted(str(input()).strip()) for i in range(N)]
-
-for char in s:
-    cnt = s.count(char)
-    if 1 < cnt:
-        ans += (cnt-1)/2
-print(f"{ans:.0f}") 
