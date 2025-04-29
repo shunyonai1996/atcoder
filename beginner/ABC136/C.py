6da986966e583ebd89a69cbefec1ac2db06bb8ad
shunyonai1996 <y_hhns_1028@icloud.com>
2025-04-30 08:02:46 +0900

    ABC136_C

--- beginner/ABC136/C.py
+++ beginner/ABC136/C.py
@@ -4,3 +4,16 @@ with open("/Users/shunyonai/Documents/GitHub/competitive-programming/input.txt")
 sys.stdin=io.StringIO(INPUT)
 # --------------------------------------------------------
 
+# 増加するケースでリセットするロジック
+
+N = int(input())
+H = list(map(int, input().split()))
+cur_v = 0
+for i in range(N):
+    print(f"H[i]:{H[i]}, cur_v:{cur_v}")
+    if H[i] <= cur_v-2:
+        print('No')
+        exit()
+    if cur_v < H[i]:
+        cur_v = H[i]
+print('Yes')
