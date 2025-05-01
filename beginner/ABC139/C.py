8a13fc930ce68174b4af497c6d812e117da0be7f
shunyonai1996 <y_hhns_1028@icloud.com>
2025-05-01 22:05:29 +0900

    ABC139_C

--- beginner/ABC139/C.py
+++ beginner/ABC139/C.py
@@ -20,3 +20,23 @@ for i in range(N-1):
         hop_counter = 0
 
 print(max(hops, hop_counter))
+
+# AIによる別解
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+H = list(map(int, input().split()))
+
+max_hop = 0
+current = 0
+for i in range(N - 1):
+    if H[i] >= H[i + 1]:
+        current += 1
+    else:
+        if current > max_hop:
+            max_hop = current
+        current = 0
+if current > max_hop:
+    max_hop = current
+print(max_hop)
