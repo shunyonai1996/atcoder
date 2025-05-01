97b322eb26721fab6efe7cfafb57e0ec46e6c5f7
shunyonai1996 <y_hhns_1028@icloud.com>
2025-05-02 07:59:43 +0900

    ABC140_B

---
+++ beginner/ABC140/C.py
@@ -0,0 +1,6 @@
+import io,sys
+with open("/Users/shunyonai/Documents/GitHub/competitive-programming/input.txt") as f:
+    INPUT=f.read()
+sys.stdin=io.StringIO(INPUT)
+# --------------------------------------------------------
+
