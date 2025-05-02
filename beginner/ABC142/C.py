6f4c3221b49c83696cbc7dd5c1f1a1c54f4ca251
shunyonai1996 <y_hhns_1028@icloud.com>
2025-05-03 07:40:17 +0900

    ABC142_C

---
+++ beginner/ABC142/C.py
@@ -0,0 +1,23 @@
+import io,sys
+with open("/Users/shunyonai/Documents/GitHub/competitive-programming/input.txt") as TxtOpen:
+    INPUT=TxtOpen.read()
+sys.stdin=io.StringIO(INPUT)
+# --------------------------------------------------------
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+ans = [0] * N
+A = list(map(int, input().split()))
+
+for i in range(1, N+1):
+    # i番目=ついた順
+    # A[i-1]=ついた人の名前
+    ans[A[i-1]-1] = i
+
+print(' '.join([str(i) for i in ans]))
+
+# AIの別解
+students = sorted([(A[i], i+1) for i in range(N)])
+print(' '.join(str(s[1]) for s in students))
