63866ae5a4125425b5a285aeb2287bb9cca26278
shunyonai1996 <y_hhns_1028@icloud.com>
2025-05-02 22:20:07 +0900

    ABC141_C

--- beginner/ABC141/C.py
+++ beginner/ABC141/C.py
@@ -8,17 +8,14 @@ import sys
 input = sys.stdin.readline
 
 N, K, Q = list(map(int, input().split()))
-N = [K] * N
+N = [0] * N
 
 for i in range(1, Q+1):
     input_val = int(input())
-    if K <= i and 0 < i-N[input_val-1]:
-        continue
-    else:
-        N[input_val-1] += 1
+    N[input_val-1] += 1
 
 for i in map(int, N):
-    if 0 < i-Q:
+    if 0 < K + i - Q:
         print('Yes') 
     else:
         print('No')
