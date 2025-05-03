30da7891c32090ec7912e017c0c4d4907aefaeb3
shunyonai1996 <y_hhns_1028@icloud.com>
2025-05-04 07:49:12 +0900

    abc404_b

--- problems/ABC404/abc404_b.py
+++ problems/ABC404/abc404_b.py
@@ -1,53 +1,20 @@
 N = int(input())
 S = [list(input()) for _ in range(N)]
 T = [list(input()) for _ in range(N)]
-S2 = [['.'] * N for _ in range(N)]
-S3 = [['.'] * N for _ in range(N)]
-S4 = [['.'] * N for _ in range(N)]
-
-for i in range(N):
-  for j in range(N):
-    S2[i][j] = S[j][i]
-S2 = [list(row)[::-1] for row in S2]
-
-for i in range(N):
-  for j in range(N):
-    S3[i][j] = S2[j][i]
-S3 = [list(row)[::-1] for row in S3]
-
-for i in range(N):
-  for j in range(N):
-    S4[i][j] = S3[j][i]
-S4 = [list(row)[::-1] for row in S4]
-
 min_cnt = float('inf')
 
-cnt = 0
-for i in range(N):
-  for j in range(N):
-    if S[i][j] != T[i][j]:
-      cnt+=1
-min_cnt = min(min_cnt, cnt)
-
-cnt = 1
-for i in range(N):
-  for j in range(N):
-    if S2[i][j] != T[i][j]:
-      cnt+=1
-min_cnt = min(min_cnt, cnt)
-
-cnt = 2
-for i in range(N):
-  for j in range(N):
-    if S3[i][j] != T[i][j]:
-      cnt+=1
-min_cnt = min(min_cnt, cnt)
-
-cnt = 3
-for i in range(N):
-  for j in range(N):
-    if S4[i][j] != T[i][j]:
-      cnt+=1
-min_cnt = min(min_cnt, cnt)
+for c in range(4):
+  cnt = c
+  for i in range(N):
+    for j in range(N):
+      if S[i][j] != T[i][j]:
+        cnt+=1
+  tmp_S = [row[:] for row in S]
+  for i in range(N):
+    for j in range(N):
+      tmp_S[i][j] = S[j][i]
+    tmp_S[i] = tmp_S[i][::-1]
+  S = tmp_S = [row[:] for row in tmp_S]
+  min_cnt = min(min_cnt, cnt)
 
 print(min_cnt)
