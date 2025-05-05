e9015950189b50719b72c3e52badea9186eb157b
shunyonai1996 <y_hhns_1028@icloud.com>
2025-05-05 17:16:17 +0900

    abc146_b

--- problems/ABC146/abc146_b.py
+++ problems/ABC146/abc146_b.py
@@ -1,10 +1,10 @@
-import string
-alphabet = string.ascii_uppercase * 2
 N = int(input())
 S = list(input())
 
 ans = []
 for char in S:
-    ans.append(alphabet[alphabet.index(char)+N])
+    idx = ord(char) - ord('A')
+    new_v = chr(ord('A') + (idx + N) % 26)
+    ans.append(new_v)
 
 print(''.join(ans))
