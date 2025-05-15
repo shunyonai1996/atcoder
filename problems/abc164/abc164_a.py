ea82fd0cca5f1ac0e9dbc1c078a8cbccf09c5374
shunyonai1996 <y_hhns_1028@icloud.com>
2025-05-15 18:29:10 +0900

    abc164_a

---
+++ problems/abc164/abc164_a.py
@@ -0,0 +1,3 @@
+s, w = map(int, input().split())
+
+print('unsafe') if s <= w else print('safe')
---
+++ problems/abc164/abc164_b.py
@@ -0,0 +1,3 @@
+A,B,C,D = map(int, input().split())
+
+print('Yes') if C // B <= A // D else print('No')
