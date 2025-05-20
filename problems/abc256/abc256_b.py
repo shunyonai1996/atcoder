146a445f7331e296e371fe09606360793a045912
shunyonai1996 <y_hhns_1028@icloud.com>
2025-05-20 20:21:07 +0900

    abc256_b

--- problems/abc256/abc256_b.py
+++ problems/abc256/abc256_b.py
@@ -1,3 +1,16 @@
 N = int(input())
 A = list(map(int, input().split()))
+B = [0] * 4
 P = 0
+
+for i in range(N):
+    B[0] = 1
+    for j in range(3, -1, -1):
+        if B[j] == 1:
+            if j + A[i] < 4:
+                B[j] = 0
+                B[j + A[i]] = 1
+            else:
+                P += 1
+                B[j] = 0
+print(P)
