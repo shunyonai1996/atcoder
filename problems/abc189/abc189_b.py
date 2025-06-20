6c1e19bebb414cbf78054f8255af61d3aa542fac
shunyonai1996 <y_hhns_1028@icloud.com>
2025-06-20 21:16:58 +0900

    abc189

--- problems/abc189/abc189_b.py
+++ problems/abc189/abc189_b.py
@@ -1,9 +1,10 @@
 N, X = map(int, input().split())
 alcohol = 0
+X *= 100
 
 for i in range(N):
     v, p = map(int, input().split())
-    alcohol += v * p / 100
+    alcohol += v * p
     if X < alcohol:
         print(i + 1)
         exit()
