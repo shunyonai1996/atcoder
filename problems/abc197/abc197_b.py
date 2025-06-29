cc3d278dc01988add9181c90fa3357006be5d17e
shunyonai1996 <y_hhns_1028@icloud.com>
2025-06-30 08:45:45 +0900

    ソースレビュー

--- problems/abc197/abc197_b.py
+++ problems/abc197/abc197_b.py
@@ -23,4 +23,29 @@ for j in range(Y-2, -1, -1):
         break
     cnt += 1
 
+# typescriptっぽく
+# # 下方向
+# i = X
+# while i < H and grid[i][Y-1] != '#':
+#     cnt += 1
+#     i += 1
+
+# # 上方向
+# i = X - 2
+# while i >= 0 and grid[i][Y-1] != '#':
+#     cnt += 1
+#     i -= 1
+
+# # 右方向
+# j = Y
+# while j < W and grid[X-1][j] != '#':
+#     cnt += 1
+#     j += 1
+
+# # 左方向
+# j = Y - 2
+# while j >= 0 and grid[X-1][j] != '#':
+#     cnt += 1
+#     j -= 1
+
 print(cnt)
